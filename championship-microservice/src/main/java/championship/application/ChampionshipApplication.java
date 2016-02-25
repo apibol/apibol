52ab6331867c98e8c8b4e5a643833328726be223
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-02-24 23:27:25 -0300

    Import on github

---
+++ championship-microservice/src/main/java/championship/application/ChampionshipApplication.java
@@ -0,0 +1,23 @@
+package championship.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Claudio E. de Oliveira on 24/02/16.
+ */
+@SpringBootApplication
+public class ChampionshipApplication implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        return Health.up().build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ChampionshipApplication.class, args);
+    }
+
+}
