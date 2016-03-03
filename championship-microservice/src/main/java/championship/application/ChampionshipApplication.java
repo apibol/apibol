4d303ab04fe3088057483bb8f8af124766f0eaa2
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-03-02 21:54:16 -0300

    dockerizing/testing discovery service

--- championship-microservice/src/main/java/championship/application/ChampionshipApplication.java
+++ championship-microservice/src/main/java/championship/application/ChampionshipApplication.java
@@ -1,14 +1,25 @@
 package championship.application;
 
+import championship.infra.DatabaseProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Claudio E. de Oliveira on 24/02/16.
  */
 @SpringBootApplication
+@Import(DatabaseProducer.class)
+@ComponentScan(basePackages = "championship")
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableHystrix
 public class ChampionshipApplication implements HealthIndicator {
 
     @Override
