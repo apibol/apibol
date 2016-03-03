567180d98c41f8e9d23ab3509fe845b3f353a574
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-03-02 22:36:36 -0300

    prediction service configure application

--- prediction-microservice/src/main/java/prediction/application/PredictionApplication.java
+++ prediction-microservice/src/main/java/prediction/application/PredictionApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import prediction.infra.DatabaseProducer;
 
 /**
  * @author Claudio E. de Oliveira on 24/02/16.
  */
 @SpringBootApplication
+@Import(DatabaseProducer.class)
+@ComponentScan(basePackages = "prediction")
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableHystrix
 public class PredictionApplication implements HealthIndicator {
 
     @Override
