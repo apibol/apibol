c23ba45f96375eff0e1009d0d679678c5814e58e
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-03-06 18:50:49 -0300

    get user information draft

---
+++ championship-microservice/src/main/java/championship/domain/User.java
@@ -0,0 +1,17 @@
+package championship.domain;
+
+import lombok.Data;
+
+/**
+ * @author Claudio E. de Oliveira on 28/02/16.
+ */
+@Data
+public class User {
+    
+    private String id;
+    
+    private String name;
+    
+    private String email;
+    
+}
---
+++ championship-microservice/src/main/java/championship/domain/service/UserInfoService.java
@@ -0,0 +1,26 @@
+package championship.domain.service;
+
+import championship.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Claudio E. de Oliveira on 06/03/16.
+ */
+@Service
+public class UserInfoService {
+
+    @Autowired @LoadBalanced
+    private RestTemplate restTemplate;
+
+    @Value("${usermicroservice.url}")
+    private String url;
+    
+    public User getUserInfo(String userId){
+        return this.restTemplate.getForObject(this.url, User.class);
+    }
+        
+}
