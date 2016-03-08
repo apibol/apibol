80489a03e5e5eed85c3ac70a40cabcc213241e9d
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-03-07 21:56:15 -0300

    enable zuul proxy server

--- api-gateway-microservice/src/main/java/gateway/application/GatewayApplication.java
+++ api-gateway-microservice/src/main/java/gateway/application/GatewayApplication.java
@@ -3,11 +3,13 @@ package gateway.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 /**
  * @author Claudio E. de Oliveira on 01/03/16.
  */
 @SpringCloudApplication
+@Controller
 @EnableZuulProxy
 public class GatewayApplication {
 
