8d91420ad561861253d4c75b0276292b4ea66243
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-03-09 20:57:45 -0300

    resource draft

--- auth-microservice/src/main/java/auth/application/AuthApplication.java
+++ auth-microservice/src/main/java/auth/application/AuthApplication.java
@@ -1,16 +1,28 @@
 package auth.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * @author Claudio E. de Oliveira on 02/03/16.
  */
-@SpringBootApplication
-@ComponentScan(basePackages = "championship")
+@SpringCloudApplication
+@RestController
+@EnableResourceServer
 public class AuthApplication implements HealthIndicator {
 
     @Override
@@ -20,6 +32,33 @@ public class AuthApplication implements HealthIndicator {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("acme")
+                    .secret("acmesecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+                    .scopes("webshop");
+        }
     }
 
 }
