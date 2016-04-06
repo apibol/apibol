72fbb6407ed30698732641ca8b3a628f78f45307
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-04-05 21:38:20 -0300

    ranking configure queue

--- prediction-microservice/src/main/java/prediction/domain/service/BattlePredictionDispatcher.java
+++ prediction-microservice/src/main/java/prediction/domain/service/BattlePredictionDispatcher.java
@@ -1,6 +1,9 @@
 package prediction.domain.service;
 
+import com.google.gson.Gson;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import prediction.domain.BattlePrediction;
 import prediction.domain.BattleResult;
 import prediction.domain.PredictionEvaluator;
@@ -10,8 +13,11 @@ import prediction.domain.repository.BattlePredictionRepository;
 import java.util.List;
 
 /**
+ * Predictions Dispatcher
  * @author Claudio E. de Oliveira on 22/03/16.
  */
+@Log4j2
+@Component
 public class BattlePredictionDispatcher {
     
     private final BattlePredictionRepository battlePredictionRepository;
@@ -26,9 +32,10 @@ public class BattlePredictionDispatcher {
 
     /**
      * Compute points by prediction
-     * @param battleResult
+     * @param battleResult - message from rabbitMQ
      */
     public void computePoints(final BattleResult battleResult){
+        log.info(String.format("[PROCESS-PREDICTIONS] Process results from game %s",battleResult.toString()));
         List<BattlePrediction> predictions = battlePredictionRepository.findByGameId(battleResult.getGameId());
         predictions.parallelStream().forEach(prediction ->{
             Integer pointsEarned = new PredictionEvaluator(battleResult).evaluate(prediction);
