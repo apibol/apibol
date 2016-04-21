814e678da5ab2f08ce9a743c463a6822a949303a
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-04-21 16:15:00 -0300

    fix some bugs

--- auth-microservice/src/main/java/auth/infra/oauth/OAuth2AuthServer.java
+++ auth-microservice/src/main/java/auth/infra/oauth/OAuth2AuthServer.java
@@ -27,16 +27,10 @@ import javax.sql.DataSource;
 public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
-    private DataSource dataSource;
-
-    @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
     private TokenStore tokenStore;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private JwtAccessTokenConverter jwtAccessTokenConverter;
@@ -51,15 +45,6 @@ public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
         oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .jdbcAuthentication()
-                .dataSource(this.dataSource)
-                .usersByUsernameQuery("select nickname,password,true from users where nickname=?")
-                .authoritiesByUsernameQuery("select nickname, scope from credentials_scope where nickname=?")
-            .passwordEncoder(this.passwordEncoder);
-    }
-
     @Override
     public void configure(ClientDetailsServiceConfigurer clients)
             throws Exception {
@@ -67,35 +52,35 @@ public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
             .inMemory()
                 .withClient("user-service")
                     .secret("4f7ec648a48b9d3fa239b497f7b6b4d8019697bd")
-                    .authorizedGrantTypes("password", "authorization_code","refresh_token", "implicit")
+                    .authorizedGrantTypes("authorization_code","refresh_token", "implicit")
                     .authorities("maintainer", "owner","user")
                     .scopes("read", "write", "trust")
                     .accessTokenValiditySeconds(1800)
             .and()
                 .withClient("event-service")
                 .secret("f7641c289245d5d43d0aa96d0c14dbc6afba31fc")
-                .authorizedGrantTypes("password", "authorization_code","refresh_token", "implicit")
+                .authorizedGrantTypes("authorization_code","refresh_token", "implicit")
                 .authorities("maintainer", "owner","user")
                 .scopes("read", "write", "trust")
                 .accessTokenValiditySeconds(1800)
             .and()
                 .withClient("predictor-service")
                 .secret("d658a112c1160fdcb5c6a4c3ce33c716cdb84160")
-                .authorizedGrantTypes("password", "authorization_code","refresh_token", "implicit")
+                .authorizedGrantTypes("authorization_code","refresh_token", "implicit")
                 .authorities("maintainer", "owner","user")
                 .scopes("read", "write", "trust")
                 .accessTokenValiditySeconds(1800)
             .and()
                 .withClient("prediction-service")
                 .secret("da29f4b2ac608bdc8024b7ab084981fdf4014c16")
-                .authorizedGrantTypes("password", "authorization_code","refresh_token", "implicit")
+                .authorizedGrantTypes("authorization_code","refresh_token", "implicit")
                 .authorities("maintainer", "owner","user")
                 .scopes("read", "write", "trust")
                 .accessTokenValiditySeconds(1800)
             .and()
                 .withClient("ranking-service")
                 .secret("e186b815cfc1a9988030a21ad8f32d8a6559f3b8")
-                .authorizedGrantTypes("password", "authorization_code","refresh_token", "implicit")
+                .authorizedGrantTypes("authorization_code","refresh_token", "implicit")
                 .authorities("maintainer", "owner","user")
                 .scopes("read", "write", "trust")
                 .accessTokenValiditySeconds(1800);
---
+++ auth-microservice/src/main/java/auth/infra/oauth/SecurityConfig.java
@@ -0,0 +1,46 @@
+package auth.infra.oauth;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Claudio E. de Oliveira on on 21/04/16.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("Defining JDBC Authentication");
+        auth.jdbcAuthentication()
+                .dataSource(this.dataSource)
+                .usersByUsernameQuery("select nickname,password,true from users where nickname=?")
+                .authoritiesByUsernameQuery("select nickname, scope from credentials_scope where nickname=?")
+                .passwordEncoder(this.passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and().httpBasic().disable().anonymous().disable().authorizeRequests().anyRequest().authenticated();
+    }
+
+}
