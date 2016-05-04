3d2bf2ced50022df52e6c0a6c3ee345cf7f0db0e
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-05-03 21:42:23 -0300

    system credentials

--- auth-microservice/src/main/java/auth/infra/oauth/OAuth2AuthServer.java
+++ auth-microservice/src/main/java/auth/infra/oauth/OAuth2AuthServer.java
@@ -20,6 +20,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 @EnableAuthorizationServer
 public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
 
+    /** One day in seconds for trusted apps */
+    private static final int ONE_DAY = 86400;
+
+    /** Three hours in seconds */
+    private static final int THREE_HOURS = 10800;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -44,40 +50,46 @@ public class OAuth2AuthServer extends AuthorizationServerConfigurerAdapter {
             throws Exception {
         clients
             .inMemory()
-                .withClient("user-service")
+                .withClient("frontend") // frontend
+                .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
+                .authorities("maintainer", "owner","user")
+                .scopes("read", "write")
+                .accessTokenValiditySeconds(THREE_HOURS)
+            .and()
+                .withClient("442cf4015509eda9c03e5ca3aceef752") // user service
                     .secret("4f7ec648a48b9d3fa239b497f7b6b4d8019697bd")
                     .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
                     .authorities("maintainer", "owner","user")
-                    .scopes("read", "write", "trust")
-                    .accessTokenValiditySeconds(1800)
+                    .scopes("trust")
+                    .accessTokenValiditySeconds(ONE_DAY)
             .and()
-                .withClient("event-service")
+                .withClient("917092f2d185310c0f4fa8ba3296e33d") // event service
                 .secret("f7641c289245d5d43d0aa96d0c14dbc6afba31fc")
                 .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
                 .authorities("maintainer", "owner","user")
-                .scopes("read", "write", "trust")
-                .accessTokenValiditySeconds(1800)
+                .scopes("trust")
+                .accessTokenValiditySeconds(ONE_DAY)
             .and()
-                .withClient("predictor-service")
+                .withClient("65dfc684722921c89ae221d90f6ae2f9") // predictor service
                 .secret("d658a112c1160fdcb5c6a4c3ce33c716cdb84160")
                 .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
                 .authorities("maintainer", "owner","user")
-                .scopes("read", "write", "trust")
-                .accessTokenValiditySeconds(1800)
+                .scopes("trust")
+                .accessTokenValiditySeconds(ONE_DAY)
             .and()
-                .withClient("prediction-service")
+                .withClient("f8374efd71270dc152a2941bd0d9bed6") // prediction service
                 .secret("da29f4b2ac608bdc8024b7ab084981fdf4014c16")
                 .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
                 .authorities("maintainer", "owner","user")
-                .scopes("read", "write", "trust")
-                .accessTokenValiditySeconds(1800)
+                .scopes("trust")
+                .accessTokenValiditySeconds(ONE_DAY)
             .and()
-                .withClient("ranking-service")
+                .withClient("47d35bda81f57a8eb8f85a410e18001b") // ranking service
                 .secret("e186b815cfc1a9988030a21ad8f32d8a6559f3b8")
                 .authorizedGrantTypes("authorization_code","refresh_token", "implicit","client_credentials")
                 .authorities("maintainer", "owner","user")
-                .scopes("read", "write", "trust")
-                .accessTokenValiditySeconds(1800);
+                .scopes("trust")
+                .accessTokenValiditySeconds(ONE_DAY);
     }
 
 }
