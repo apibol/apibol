c01ebb2d82823e7253a4d6ec8531f5041bb268dd
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-05-26 13:00:32 -0300

    improve ranking log

--- ranking-microservice/src/main/java/ranking/domain/service/UpdateUserPointsService.java
+++ ranking-microservice/src/main/java/ranking/domain/service/UpdateUserPointsService.java
@@ -1,5 +1,6 @@
 package ranking.domain.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ranking.domain.RequestUpdateUserPoints;
@@ -9,6 +10,7 @@ import ranking.domain.repository.RankingRepository;
  * @author Claudio E. de Oliveira on 23/03/16.
  */
 @Service
+@Log4j2
 public class UpdateUserPointsService {
 
     private final RankingRepository rankingRepository;
@@ -23,6 +25,7 @@ public class UpdateUserPointsService {
      * @param request
      */
     public void update(RequestUpdateUserPoints request){
+        log.info("[RECEIVE-UPDATE-USER-POINTS] Update user points. data: "+ request.toString());
         this.rankingRepository.updatePoints(request);
     }
 
