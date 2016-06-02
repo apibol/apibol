bdedf09e09ef2b373be6914895a151e5d0128163
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-06-02 20:33:32 -0300

    docker adjustments

--- user-microservice/src/main/java/user/infra/data/DataSourceProducer.java
+++ user-microservice/src/main/java/user/infra/data/DataSourceProducer.java
@@ -3,6 +3,7 @@ package user.infra.data;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -16,6 +17,7 @@ import javax.sql.DataSource;
 public class DataSourceProducer {
 
     @Bean
+    @Profile(value = "default")
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
@@ -27,8 +29,27 @@ public class DataSourceProducer {
     }
 
     @Bean
+    @Profile(value = "docker")
+    public DataSource containerDataSource() {
+        final BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://dbinstance:3306/user");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        dataSource.setMaxTotal(10);
+        return dataSource;
+    }
+
+    @Bean
+    @Profile(value = "default")
     public JdbcTemplate jdbcTemplate(){
         return new JdbcTemplate(this.dataSource());
+    }
+
+    @Bean
+    @Profile(value = "docker")
+    public JdbcTemplate containerJdbcTemplate(){
+        return new JdbcTemplate(this.containerDataSource());
     }
 
 }
