03138ad623669850ef68993e9a574e57babe9d0e
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2016-06-04 10:52:11 -0300

    some frontend bootstrap fixes

--- predictor-microservice/src/main/java/predictor/domain/service/PredictorService.java
+++ predictor-microservice/src/main/java/predictor/domain/service/PredictorService.java
@@ -4,14 +4,12 @@ import domain.SystemUser;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import predictor.domain.Event;
 import predictor.domain.JoinPredictorRequest;
 import predictor.domain.Participant;
 import predictor.domain.Predictor;
-import predictor.domain.exception.InvalidPredictor;
-import predictor.domain.exception.ParticipantIsNotOwnerException;
-import predictor.domain.exception.ParticipantNotInPredictor;
-import predictor.domain.exception.PredictorNotFound;
+import predictor.domain.exception.*;
 import predictor.domain.repository.PredictorRepository;
 import predictor.domain.resource.JoinPredictorDTO;
 import predictor.domain.resource.PredictorDTO;
@@ -37,7 +35,7 @@ public class PredictorService {
     private final SystemUserService systemUserService;
 
     @Autowired
-    public PredictorService(ParticipantService participantService, EventService eventService, PredictorRepository predictorRepository,SystemUserService systemUserService) {
+    public PredictorService(ParticipantService participantService, EventService eventService, PredictorRepository predictorRepository, SystemUserService systemUserService) {
         this.participantService = participantService;
         this.eventService = eventService;
         this.predictorRepository = predictorRepository;
@@ -54,8 +52,8 @@ public class PredictorService {
         log.info("[CREATE-PREDICTOR] Creating predictor ");
         Participant newParticipant = this.participantService.getUserInfo(predictorDTO.getUserId());
         Event event = this.eventService.getEventInfo(predictorDTO.getEventId());
-        Predictor predictor = Predictor.createPredictor(UUID.randomUUID().toString(),event.getId(), newParticipant,event.getOpen());
-        predictor.join(JoinPredictorRequest.create(newParticipant,""));
+        Predictor predictor = Predictor.createPredictor(UUID.randomUUID().toString(), event.getId(), newParticipant, event.getOpen());
+        predictor.join(JoinPredictorRequest.create(newParticipant, ""));
         predictor = this.predictorRepository.save(predictor);
         log.info("[CREATE-PREDICTOR] Predictor created with success ");
         return predictor;
@@ -69,13 +67,13 @@ public class PredictorService {
      * @param joinPredictorDTO
      * @return
      */
-    public Predictor join(String predictorId,String hash, JoinPredictorDTO joinPredictorDTO) {
+    public Predictor join(String predictorId, String hash, JoinPredictorDTO joinPredictorDTO) {
         log.info(String.format("[ADD-PARTICIPANT] Adding participant %s in predictor %s ", joinPredictorDTO.getUserId(), predictorId));
         final SystemUser loggerUserInfo = this.systemUserService.loggerUserInfo(joinPredictorDTO.getUserId());
         Participant newParticipant = this.participantService.getUserInfo(loggerUserInfo.getId());
         Predictor predictor = this.predictorRepository.findOne(predictorId);
         if (Objects.nonNull(predictor)) {
-            predictor.join(JoinPredictorRequest.create(newParticipant,hash));
+            predictor.join(JoinPredictorRequest.create(newParticipant, hash));
             predictor = this.predictorRepository.save(predictor);
             log.info(String.format("[ADD-PARTICIPANT] Participant %s added in predictor %s ", joinPredictorDTO.getUserId(), predictorId));
             return predictor;
@@ -102,7 +100,7 @@ public class PredictorService {
      */
     public Predictor findOne(String id) {
         final Predictor predictor = this.predictorRepository.findOne(id);
-        if (Objects.isNull(predictor)){
+        if (Objects.isNull(predictor)) {
             log.info(String.format("[GET-PREDICTOR-ID] Predictor %s not found in database", id));
             throw new PredictorNotFound(id);
         }
@@ -118,12 +116,12 @@ public class PredictorService {
     public void deletePredictor(String id, String name) {
         final SystemUser systemUser = this.systemUserService.loggerUserInfo(name);
         final Predictor predictor = this.predictorRepository.findOne(id);
-        if(new IsPredictorOwner(predictor).isSatisfiedBy(systemUser)){
-            log.info(String.format("[DELETE-PREDICTOR] Delete predictor by id. Predictor %s by user %s", id,name));
+        if (new IsPredictorOwner(predictor).isSatisfiedBy(systemUser)) {
+            log.info(String.format("[DELETE-PREDICTOR] Delete predictor by id. Predictor %s by user %s", id, name));
             this.predictorRepository.delete(id);
-        }else{
-            log.error(String.format("[DELETE-PREDICTOR] User %s is not owner on predictor %s ", name,id));
-            throw new ParticipantIsNotOwnerException(id,systemUser.getId());
+        } else {
+            log.error(String.format("[DELETE-PREDICTOR] User %s is not owner on predictor %s ", name, id));
+            throw new ParticipantIsNotOwnerException(id, systemUser.getId());
         }
     }
 
@@ -137,7 +135,7 @@ public class PredictorService {
     public Participant findByPredictorAndParticipantId(String predictorId, String participantId) {
         Predictor predictor = this.predictorRepository.findByIdAndParticipantsId(predictorId, participantId);
         if (Objects.isNull(predictor)) {
-            log.error(String.format("[GET-PARTICIPANT-IN-PREDICTOR] Participant %s is not in predictor %s ", participantId,predictorId));
+            log.error(String.format("[GET-PARTICIPANT-IN-PREDICTOR] Participant %s is not in predictor %s ", participantId, predictorId));
             throw new ParticipantNotInPredictor(participantId, predictorId);
         } else {
             return predictor.participantInfo(participantId);
@@ -150,8 +148,8 @@ public class PredictorService {
      * @param predictor
      * @return
      */
-    public Predictor saveByEvent(Predictor predictor){
-        log.info("[SAVE-NEW-PREDICTOR-BY-EVENT] Receive new event "+ predictor.getId());
+    public Predictor saveByEvent(Predictor predictor) {
+        log.info("[SAVE-NEW-PREDICTOR-BY-EVENT] Receive new event " + predictor.getId());
         return this.predictorRepository.save(predictor);
     }
 
@@ -161,10 +159,15 @@ public class PredictorService {
      * @param userId
      * @return
      */
-    public List<Predictor> myPredictors(String userId){
+    public List<Predictor> myPredictors(String userId) throws NoPredictorsFound {
         log.info(String.format("[MY-PREDICTORS] Retrieve my predictors user %s", userId));
         final SystemUser loggerUserInfo = this.systemUserService.loggerUserInfo(userId);
-        return this.predictorRepository.findByInvitationsUserId(loggerUserInfo.getId());
+        final List<Predictor> myPredictors = this.predictorRepository.findByInvitationsUserId(loggerUserInfo.getId());
+        if (CollectionUtils.isEmpty(myPredictors)) {
+            log.info("[My-PREDICTORS] No predictors for user " + userId);
+            throw new NoPredictorsFound(userId);
+        }
+        return myPredictors;
     }
 
 }
